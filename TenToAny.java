943c4dc049cff86655931c90dae632a74ba33604
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-03-15 22:14:07 -0700

    Update TenToAny.java

--- TenToAny.java
+++ TenToAny.java
@@ -9,16 +9,41 @@ public class TenToAny
    private int base10;
    private int newBase;
 
-	//add constructors
+ public void setBN(int base10, int newBase) {
+        this.base10 = base10;
+        this.newBase = newBase;
+    }
 
-	//add a set method
+    private String answer;
 
 	public String getNewNum()
 	{
-		String newNum="";
-		return newNum;
+	int number = base10;
+        answer = "";
+        while (number != 0) {
+            int val = number % newBase;
+            if (val < 10) {
+                answer = val + answer;
+            } else if (val == 10) {
+                answer = "A" + answer;
+            } else if (val == 11) {
+                answer = "B" + answer;
+            } else if (val == 12) {
+                answer = "C" + answer;
+            } else if (val == 13) {
+                answer = "D" + answer;
+            } else if (val == 14) {
+                answer = "E" + answer;
+            } else if (val == 15) {
+                answer = "F" + answer;
+            }
+            number = number / newBase;
+        }
+        return answer;
 	}
 
-	//add a toString method	
+  public String toString() {
+        return base10 + " base 10 is " + answer + " in base " + newBase;
+    }
 	
 }
