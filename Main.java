1cd4a3718d9f92df9a79aab06f2593b4ed146ce2
jamesahn0720 <76964759+jamesahn0720@users.noreply.github.com>
2021-05-16 23:06:11 -0700

    Update Main.java

--- Main.java
+++ Main.java
@@ -3,6 +3,21 @@ class Main {
    TenToAny tenToAny = new TenToAny();
         tenToAny.setBN(234, 9);
         tenToAny.getNewNum();
+    
+        tenToAny.setBN(100, 2);
+        tenToAny.getNewNum();
+    
+      tenToAny.setBN(10, 2);
+      tenToAny.getNewNum(); 
+    
+      tenToAny.setBN(15, 2);
+      tenToAny.getNewNum();
+    
+      tenToAny.setBN(256, 2);
+      tenToAny.getNewNum();
+    
+      tenToAny.setBN(100, 8);
+      tenToAny.getNewNum();
         System.out.println(tenToAny.toString());
   }
 }
